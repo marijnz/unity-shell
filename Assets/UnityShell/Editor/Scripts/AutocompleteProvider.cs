e0d484a13349a3de54029ab933df7a2ef0a6640b
Yilmaz Kiymaz <yilmaz.kiymaz@innogames.com>
2018-07-03 11:41:40 +0200

    Add support for .NET 4.x Scripting Runtime Version

--- Assets/UnityShell/Editor/Scripts/AutocompleteProvider.cs
+++ Assets/UnityShell/Editor/Scripts/AutocompleteProvider.cs
@@ -1,10 +1,6 @@
 ﻿using System;
-using System.Diagnostics;
-using System.Linq;
 using System.Threading;
 using Mono.CSharp;
-using UnityEngine;
-using Debug = UnityEngine.Debug;
 
 namespace UnityShell
 {
@@ -18,19 +14,28 @@ namespace UnityShell
 		public void SetInput(string input)
 		{
 			handleCount++;
-			new Thread(() =>
+			new Thread((threadStart) =>
 			{
 				int handle = handleCount;
 
 				if(!string.IsNullOrEmpty(input))
 				{
 					string prefix;
+#if NET_4_6 || NET_STANDARD_2_0
+					var evaluator = new Evaluator(new CompilerContext(new CompilerSettings(), new ConsoleReportPrinter()));
+					var result = evaluator.GetCompletions(input, out prefix);
+#else
 					var result = Evaluator.GetCompletions(input, out prefix);
+#endif
 
 					// Avoid old threads overriding with old results
 					if(handle == handleCount)
 					{
 						completions = result;
+						if (completions == null)
+						{
+							return;
+						}
 						for (var i = 0; i < completions.Length; i++)
 						{
 							completions[i] = input + completions[i];
--- Assets/UnityShell/Editor/Scripts/ShellEvaluator.cs
+++ Assets/UnityShell/Editor/Scripts/ShellEvaluator.cs
@@ -6,7 +6,9 @@ namespace UnityShell
 	[Serializable]
 	public class ShellEvaluator
 	{
-		Evaluator evaluator;
+#if NET_4_6 || NET_STANDARD_2_0
+		Evaluator evaluator = new Evaluator(new CompilerContext(new CompilerSettings(), new ConsoleReportPrinter()));
+#endif
 
 		public ShellEvaluator()
 		{
@@ -14,14 +16,22 @@ namespace UnityShell
 			{
 				try
 				{
+#if NET_4_6 || NET_STANDARD_2_0
+					evaluator.ReferenceAssembly(assembly);
+#else
 					Evaluator.ReferenceAssembly(assembly);
+#endif
 				}
 				catch (Exception)
 				{
 					// ignored
 				}
 			}
+#if NET_4_6 || NET_STANDARD_2_0
+			evaluator.Run ("using UnityEngine; using UnityEditor; using System; using System.Collections.Generic;");
+#else
 			Evaluator.Run ("using UnityEngine; using UnityEditor; using System; using System.Collections.Generic;");
+#endif
 		}
 
 		public object Evaluate(string command)
@@ -31,7 +41,11 @@ namespace UnityShell
 				command += ";";
 			}
 
+#if NET_4_6 || NET_STANDARD_2_0
+			var compilationResult = evaluator.Compile(command);
+#else
 			var compilationResult = Evaluator.Compile(command);
+#endif
 			if (compilationResult == null)
 			{
 				return "Compilation failed";
